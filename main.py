19dce8a679650fbe1a082fe601ba882746aff76b
dmitri1998171 <dmitri1998171@icloud.com>
2024-07-05 12:23:17 +0300

    get & parse a time

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 '''
 TODO
 
-1) get time
-2) collect emodji
-3) parse time
++ 1) get a time
++ 2) parse a time
+3) collect emodji
 4) show the emodji
 
 '''
+
+import datetime
+
+now = str(datetime.datetime.now().time())       # get a time in str
+now = now.split('.')                            # delete millis
+time = now[0]
+print(time)
+
+time = time.split(":")                          
+time.pop()                                      # delete seconds
+print(time)
+
 
